15256c7a9a0645854ab037f00836640c820f5457
Žiga Deisinger <41201503+zigad@users.noreply.github.com>
2020-01-01 23:17:25 +0100

    Final version

--- remaining_time/src/Service/RemainingTimeService.php
+++ remaining_time/src/Service/RemainingTimeService.php
@@ -15,11 +15,16 @@ class RemainingTimeService {
     $eventStartTime = strtotime($startDate->format(self::DATE_FORMAT));
     $currentTime = strtotime($now->format(self::DATE_FORMAT));
     $lenght = 3600;
+    $eventEndDate = $eventStartTime + $lenght;
     $razlika = floor(($eventStartTime - $currentTime));
     $days = floor(($eventStartTime - $currentTime) /86400);
     $hours = floor(($eventStartTime - $currentTime) %86400/3600);
     $minutes = floor(($eventStartTime - $currentTime) %3600/60);
     $seconds = floor(($eventStartTime - $currentTime) %60);
+    $days_end = floor(($eventEndDate - $currentTime) /86400);
+    $hours_end = floor(($eventEndDate - $currentTime) %86400/3600);
+    $minutes_end = floor(($eventEndDate - $currentTime) %3600/60);
+    $seconds_end = floor(($eventEndDate - $currentTime) %60);
     if (!empty($eventDate)) {
       switch (true) {
         case ( $currentTime < $eventStartTime ):
@@ -27,7 +32,7 @@ class RemainingTimeService {
           break;
         case ( ($currentTime >= $eventStartTime) && ($currentTime <= ($eventStartTime + $lenght))):
         
-          $result = ('This event is happening right now!');
+          $result = ('This event is happening right now and will end in: ').$days_end.(' days, ').$hours_end.(' hours ').$minutes_end.(' minutes ').$seconds_end.(' seconds');
           break;
 
         case ( $currentTime > ($eventStartTime + $lenght)):
